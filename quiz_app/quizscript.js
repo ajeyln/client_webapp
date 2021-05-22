31abbca1dacb269db43ef04a2397f343bf2e90bf
Ajeya Nayak <ajeyanayak@gmail.com>
2021-05-23 01:52:52 +0530

    Quiz app

--- quiz_app/quizscript.js
+++ quiz_app/quizscript.js
@@ -1,114 +1,75 @@
-const questionSets =[{
-                    "Question": "1. India is a federal union comprising twenty-nine states and how many union territories?",
-                    "a": "6",
-                    "b": "7",
-                    "c": "8",
-                    "d": "9",
-                    "ans": "answer4",
-                }, {
-                    "Question": "2. Which of the following is the capital of Arunachal Pradesh?",
-                    "a": "Itanagar",
-                    "b": "Dispur",
-                    "c": "Imphal",
-                    "d": "Panaji",
-                    "ans": "answer1",
-                },{
-                    "Question": "3. What are the major languages spoken in Andhra Pradesh?",
-                    "a": "English and Telugu",
-                    "b": "Telugu and Urdu",
-                    "c": "Telugu and Kannada",
-                    "d": "All of the above languages",
-                    "ans": "answer2",
-                },{
-                    "Question": "4. What is the state flower of Haryana?",
-                    "a": "Lotus",
-                    "b": "Rhododendron",
-                    "c": "Golden Shower",
-                    "d": "Not declared",
-                    "ans": "answer1",
-                }, {
-                    "Question": "5. Which of the following states is not located in the North?",
-                    "a": "Jharkhand",
-                    "b": "Jammu and Kashmir",
-                    "c": "Himachal Pradesh",
-                    "d": "Haryana",
-                    "ans": "answer1",
-                }, {
-                    "Question": "6. In which state is the main language Khasi?",
-                    "a": "Mizoram",
-                    "b": "Nagaland",
-                    "c": "Meghalaya",
-                    "d": "Tripura",
-                    "ans": "answer3",
-                }, {
-                    "Question": "7. Which is the largest coffee producing state of India?",
-                    "a": "Kerala",
-                    "b": "Tamil Nadu",
-                    "c": "Karnataka",
-                    "d": "Arunachal Pradesh",
-                    "ans": "answer3",
-                }, {
-                    "Question": "8. Which state has the largest area?",
-                    "a": "Maharashtra",
-                    "b": "Madhya Pradesh",
-                    "c": "Uttar Pradesh",
-                    "d": "Rajasthan",
-                    "ans": "answer4",
-                }, {
-                    "Question": "9. Which state has the largest population?",
-                    "a": "Uttar Pradesh",
-                    "b": "Maharastra",
-                    "c": "Bihar",
-                    "d": "West Bengal",
-                    "ans": "answer1",
-                }, {
-                    "Question": "10. In what state is the Elephant Falls located?",
-                    "a": "Mizoram",
-                    "b": "Orissa",
-                    "c": "Manipur",
-                    "d": "Meghalaya",
-                    "ans": "answer4",
-                }, ];
+const questionNumber = document.querySelector(".question-number")
+const questionText = document.querySelector(".question-text")
+const optioncontainer = document.querySelector(".option-container")
 
-const question = document.querySelector(".question");
-const option1 = document.querySelector("#option1");
-const option2 = document.querySelector("#option2");
-const option3 = document.querySelector("#option3");
-const option4 = document.querySelector("#option4");
-const submit = document.querySelector("#submit");
+let questionCounter = 0;
+let currentQuestion;
+let availableQuestions = [];
+let availableOptions = [];
 
-const answers = document.querySelectorAll(".answers");
-
-const showscore = document.querySelector("#showscore");
-
-let questioncount = 0;
-let score = 0;
-
-const loadQuestion = () => {
-    const questionlist = questionSets[questioncount]
-    question.innerHTML = questionlist.Question;
-    option1.innerHTML = questionlist.a;
-    option2.innerHTML = questionlist.b;
-    option3.innerHTML = questionlist.c;
-    option4.innerHTML = questionlist.d;
+function setAvailableQuestions(){
+    const totalQuestion = question_set.length;
+    for(let i=0; i<totalQuestion; i++){
+        availableQuestions.push(question_set[i])
+    }
 }
 
-loadQuestion();
+function getNewQuestion(){
+    questionNumber.innerHTML = "Question " + (questionCounter + 1) + " of " + question_set.length
+    const questionIndex = availableQuestions[Math.floor(Math.random()*availableQuestions.length)]
+    currentQuestion = questionIndex;
+    questionText.innerHTML = currentQuestion.question;
+    const index1= availableQuestions.indexOf(questionIndex);
+    availableQuestions.splice(index1,1);
+    const optionLen = currentQuestion.options.length
+    for(let i=0; i<optionLen; i++){
+        availableOptions.push(i)
+    }
+    optioncontainer.innerHTML = " ";
+    let animationDelay = 0.15;
+    for(let i=0; i<optionLen; i++){
+        const optonIndex = availableOptions[Math.floor(Math.random() * availableOptions.length)];
+        const index2 = availableOptions.indexOf(optonIndex);
+        availableOptions.splice(index2,1);
+        const option = document.createElement("div");
+        option.innerHTML = currentQuestion.options[optonIndex];
+        option.id = optonIndex;
+        option.style.animationDelay = animationDelay + "s";
+        animationDelay = animationDelay + 0.15;
+        option.className = "option";
+        optioncontainer.appendChild(option)
+        option.setAttribute("onclick", "getResult(this)");
+    } 
+    questionCounter++
+}
 
-const get_checked_answer = () => {
-    let answer;
-    answers.forEach((curAnsElem) => {
-        if(curAnsElem.checked){
-            answer = curAnsElem.id;
+function getResult(element){
+    const id = parseInt(element.id);
+    if(id === 0){
+        element.classList.add("correct");
+    }else{
+        element.classList.add("wrong");
+        const optionLen = optioncontainer.children.length;
+        for(let i=0 ; i<optionLen; i++){
+            if(parseInt(optioncontainer.children[i].id) === 0){
+                optioncontainer.children[i].classList.add("correct");
+            }
         }
-    });
-    return answer
-};
+    }
+    unclickableOptions();
+}
+
+function unclickableOptions(){
+    const optionLen = optioncontainer.children.length;
+    for(let i=0 ; i<optionLen; i++){
+        optioncontainer.children[i].classList.add("already-answered");
+    }
+}
 
 submit.addEventListener("click", () => {
-    const checked_answer = get_checked_answer();
-    console.log(checked_answer)
-    if(checked_answer === questionSets[questioncount].ans){
+    const id = parseInt(element.id);
+    if(id === 0){
+        element.classList.add("correct");
         score++;
     };
     questioncount++;
@@ -122,3 +83,8 @@ submit.addEventListener("click", () => {
         showscore.classList.remove("scorearea")
     }
 });
+
+window.onload = function(){
+    setAvailableQuestions();
+    getNewQuestion();
+}
